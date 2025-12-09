9054abe8038066f715f1770c43aeda24f6a1a9b8
Usma Aktar <usma.cse101@gmail.com>
2025-12-09 18:25:36 +0100

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas
 import numpy
 import matplotlib.pyplot as plt
-from sklearn.preprocessiong import StandardScaler
+from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 
 
