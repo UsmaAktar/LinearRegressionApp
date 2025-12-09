a00885e033a0220b6c66e95de7bb25ada490ce28
Usma Aktar <usma.cse101@gmail.com>
2025-12-09 18:23:45 +0100

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -1,3 +1,10 @@
 import streamlit as st
+import pandas
+import numpy
+import matplotlib.pyplot as plt
+from sklearn.preprocessiong import StandardSclar
+from sklearn.model_selection import train_test_split
+
 
 st.title("Linear Regression Web Application")
+st.subheader("Data Science with Usma Aktar")
