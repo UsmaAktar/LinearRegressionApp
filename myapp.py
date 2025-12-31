ba8b627277bc367b354fd3c73877119e0c26b8b1
Usma Aktar <usma.cse101@gmail.com>
2025-12-31 02:23:54 +0100

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -49,7 +49,7 @@ df = df[features + [target]].dropna()
 X = df[features]
 y = df[target]
 
-scaler = StandardScalar()
+scaler = StandardScaler()
 X_scaled = scaler.fit_transform(X)
 
 X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.2, random_state=42)
